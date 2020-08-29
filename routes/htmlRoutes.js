50986dcdc145cdc05dcebf57c2be3267b0c4a981
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-08-29 13:48:24 -0700

    Updating to add comments and add test data to note file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,12 @@
 const path = require("path");
-//const router = require("express").Router();
-// exporting html paths
+
 module.exports = function(app) {
-    
-    
+    //Sends user to note taker page
     app.get("/notes", function(req,res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
-   // set index.html as default page
-   
+   // sets index.html as default page
    app.get("*", function(req,res) {
        res.sendFile(path.join(__dirname, "../public/index.html"));
      });
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+
+//express setup
 const express = require("express");
-
 const app = express();
-
 const PORT = process.env.PORT || 8080;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("./public/"));
 
+//Here are my routes to the note page and then the html page. 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
