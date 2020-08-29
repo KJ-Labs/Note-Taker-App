7cfd702bdb53cc507ea7e8f4a02bee4151b4bed5
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-08-29 13:44:09 -0700

    Updating to add delete functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,36 +3,42 @@ var notesData = getNotes();
 
 function getNotes() {
     let data = fs.readFileSync('db/db.json', 'utf8');
-
     let notes = JSON.parse(data);
-
-    // Give each note an ID that matches its index (this gets run for every time the page is refreshed)
+    // Give each note an ID, so that it can be deleted/opened later. 
     for (let i = 0; i < notes.length; i++) {
         notes[i].id = '' + i;
     }
-
     return notes;
 }
 
-
 module.exports = function(app) {
-
+  //retreives the input from the user so that it can show up online
     app.get("/api/notes", function (req, res) {
         notesData = getNotes();
         res.json(notesData);
     });
 
-    
+    //pushes the input to the notes file, so that it can be seen in that and online. 
     app.post("/api/notes/", function(req,res) {
-        note_data.push(req.body);
+      notesData.push(req.body);
         fs.writeFileSync('db/db.json', JSON.stringify(notesData), 'utf8');
         res.json(true);
     })
 
-    app.delete("/api/notes/", function(req,res) {
-        note_data.length = 0;
-        res.json({ok: true});
-    })
+    app.delete("/api/notes/:id", function (req, res) {
+      //gets the id of the note
+      const requestID = req.params.id;
+      let note = notesData.filter(note => {
+          return note.id === requestID;
+      })[0];
+      //gets the index of the note they want to delete
+      const index = notesData.indexOf(note);
+      //removes the note that they've selected. 
+      notesData.splice(index, 1);
+      //Updates the notes file log. 
+      fs.writeFileSync('db/db.json', JSON.stringify(notesData), 'utf8');
+      res.json("Note deleted");
+  });
 
 };
 
