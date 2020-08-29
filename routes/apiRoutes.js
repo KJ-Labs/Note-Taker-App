8e24784e731a08c1a6599ca562b4479cc311c8fa
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-08-28 21:02:42 -0700

    Updating to try to get note to show up

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,9 +3,24 @@ const note_data = require("../db/note_data");
 // exporting html paths
 module.exports = function(app) {
 
-    app.get("/api/notes/", function(req,res) {
-        res.json(note_data);
+    app.get("/api/notes", function(req,res) {
+        return res.json(note_data);
     });
+
+// Displays a single character, or returns false
+app.get("/api/notes/:id", function(req, res) {
+    var chosen = req.params.note_data;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < note_data.length; i++) {
+      if (chosen === note_data[i].routeName) {
+        return res.json(note_data[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
     
     app.post("/api/notes/", function(req,res) {
         note_data.push(req.body);
